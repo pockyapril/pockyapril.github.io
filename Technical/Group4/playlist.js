2b05fde4001737b6256dd67ee6de2c08b05786e0
pockyapril <155592942+pockyapril@users.noreply.github.com>
2024-05-21 05:59:50 -0500

    music error fix

--- Technical/Group4/playlist.js
+++ Technical/Group4/playlist.js
@@ -1,10 +1,10 @@
 // Whole playlist
 var playlist = [
     { title: "Will I Ever See You Again?", file: "../Music/Song1.mp3" },
-    { title: "august", file: "../Music/Song2.mp3" },
-    { title: "Holssi", file: "../Music/Song3.mp3" },
-    { title: "Weekend", file: "../Music/Song4.mp3" },
-    { title: "Womxnly", file: "../Music/Song5.mp3" },
+    { title: "august", file: "/Music/Song2.mp3" },
+    { title: "Holssi", file: "/Music/Song3.mp3" },
+    { title: "Weekend", file: "/Music/Song4.mp3" },
+    { title: "Womxnly", file: "/Music/Song5.mp3" },
 ];
 
 var currentSong = 0;
